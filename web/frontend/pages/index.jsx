54a9b2a99728429b8286a85db935074c0232d186
Javier Gongora <jgongora@gmail.com>
2022-10-04 08:52:54 -0700

    Add mock data to QRCodes

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -21,7 +21,38 @@ export default function HomePage() {
   */
   const isLoading = false;
   const isRefetching = false;
-  const QRCodes = [];
+  const QRCodes = [
+    {
+      createdAt: "2022-06-13",
+      destination: "checkout",
+      title: "My first QR code",
+      id: 1,
+      discountCode: "SUMMERDISCOUNT",
+      product: {
+        title: "Faded t-shirt",
+      }
+    },
+    {
+      createdAt: "2022-06-13",
+      destination: "product",
+      title: "My second QR code",
+      id: 2,
+      discountCode: "WINTERDISCOUNT",
+      product: {
+        title: "Cozy parka",
+      }
+    },
+    {
+      createdAt: "2022-06-13",
+      destination: "product",
+      title: "QR code for deleted product",
+      id: 3,
+      product: {
+        title: "Deleted product",
+      }
+    },
+  ];
+  
 
   /* Set the QR codes to use in the list */
   const qrCodesMarkup = QRCodes?.length ? (
