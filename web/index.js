111b2111bd5d37d04863e930c50b61c259471100
Javier Gongora <jgongora@gmail.com>
2022-10-04 09:53:01 -0700

    Delete temporary backend endpoint

--- web/index.js
+++ web/index.js
@@ -119,25 +119,6 @@ export async function createServer(
     res.status(200).send(countData);
   });
 
-  app.get("/api/products/create", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(
-      req,
-      res,
-      app.get("use-online-tokens")
-    );
-    let status = 200;
-    let error = null;
-
-    try {
-      await productCreator(session);
-    } catch (e) {
-      console.log(`Failed to process products/create: ${e.message}`);
-      status = 500;
-      error = e.message;
-    }
-    res.status(status).send({ success: status === 200, error });
-  });
-
   // All endpoints after this point will have access to a request.body
   // attribute, as a result of the express.json() middleware
   app.use(express.json());
